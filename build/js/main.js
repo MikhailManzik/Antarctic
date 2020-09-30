dc7f64177f93236ce73668070333a64fa006bbd9
MikhailManzik <mikhailmanzik@yandex.ru>
2020-09-30 15:35:33 +0200

    Добавляет папку build.

---
+++ build/js/main.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var buttonMenu = document.querySelector('.toggle');
+var siteNavigation = document.querySelector('.main-nav');
+var logo = document.querySelector('.logo__img');
+var navigationMobile = document.querySelector('.nav-mobile');
+
+buttonMenu.classList.remove('toggle--hidden');
+logo.classList.remove('logo__img--fill-color');
+navigationMobile.classList.add('nav-mobile--js');
+
+buttonMenu.addEventListener('click', function () {
+  if (buttonMenu.classList.contains('toggle--opened')) {
+    buttonMenu.classList.remove('toggle--opened');
+    buttonMenu.classList.add('toggle--closed');
+    logo.classList.remove('logo__img--fill-color');
+  } else {
+    buttonMenu.classList.remove('toggle--closed');
+    buttonMenu.classList.add('toggle--opened');
+    logo.classList.add('logo__img--fill-color');
+  }
+});
+
+siteNavigation.classList.add('main-nav--closed');
+buttonMenu.addEventListener('click', function () {
+  if (siteNavigation.classList.contains('main-nav--closed')) {
+    siteNavigation.classList.remove('main-nav--closed');
+  } else {
+    siteNavigation.classList.add('main-nav--closed');
+  }
+});
+
