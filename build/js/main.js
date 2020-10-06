2b833731dde23e6fe2268441889593a8dc462515
MikhailManzik <mikhailmanzik@yandex.ru>
2020-10-06 16:49:35 +0200

    Обновляет файлы в папке build.

--- build/js/main.js
+++ build/js/main.js
@@ -4,10 +4,14 @@ var buttonMenu = document.querySelector('.toggle');
 var siteNavigation = document.querySelector('.main-nav');
 var logo = document.querySelector('.logo__img');
 var navigationMobile = document.querySelector('.nav-mobile');
+var checkboxForm = document.querySelector('#checkbox-personal-data');
+var checkboxBlock = document.querySelector('.checkbox');
+var buttonSubmitForm = document.querySelector('.order__button');
+var checkboxLabel = checkboxBlock.querySelector('span');
 
 buttonMenu.classList.remove('toggle--hidden');
 logo.classList.remove('logo__img--fill-color');
-navigationMobile.classList.add('nav-mobile--js');
+navigationMobile.classList.remove('nav-mobile--js');
 
 buttonMenu.addEventListener('click', function () {
   if (buttonMenu.classList.contains('toggle--opened')) {
@@ -29,3 +33,17 @@ buttonMenu.addEventListener('click', function () {
     siteNavigation.classList.add('main-nav--closed');
   }
 });
+
+var addColorChceckboxLabel = function () {
+  if (!checkboxForm.checked) {
+    checkboxLabel.style.color = 'red';
+  }
+};
+
+var removeColorChceckboxLabel = function () {
+  checkboxLabel.style.color = '#ffffff';
+};
+
+buttonSubmitForm.addEventListener('click', addColorChceckboxLabel);
+checkboxForm.addEventListener('input', removeColorChceckboxLabel);
+
